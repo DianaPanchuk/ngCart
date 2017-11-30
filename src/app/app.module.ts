1c0133e85f6de5c741b5a16d74df3de237c06926
Diana Panchuk <dianochkaua@mail.ru>
2017-11-30 21:57:00 +0200

    remove routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Ng2Webstorage } from 'ngx-webstorage';
 import { StoreService } from './store.service';
@@ -17,7 +15,6 @@ import { FilterPipe } from './filter.pipe';
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
-    AppRoutingModule,
     Ng2Webstorage
   ],
   providers: [StoreService],
