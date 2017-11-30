047ff990b2d1acafbb232864b6175d6258a64cba
Diana Panchuk <dianochkaua@mail.ru>
2017-11-30 16:54:06 +0200

    cart form, display cart list (store data in variable)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { cartItem } from './cartItem.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  cartItems: cartItem[] = [];
+  cartFrom: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.initCartForm();
+  }
+
+  initCartForm() {
+    this.cartFrom = new FormGroup({
+      id: new FormControl(null),
+      name: new FormControl(null, Validators.required),
+      price: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^\d+(?:[.,]\d+)*$/)]
+      ),
+      amount: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^[0-9]\d*$/)]
+      )
+    });
+  }
+
+  onSubmit() {
+    this.cartItems.push(
+      new cartItem(
+        new Date(),
+        this.cartFrom.get('name').value,
+        this.cartFrom.get('price').value,
+        this.cartFrom.get('amount').value
+      )
+    );
+    this.cartFrom.reset();
+  }
 }
---
+++ src/app/cartItem.model.ts
@@ -0,0 +1,13 @@
+export class cartItem {
+  constructor(
+    private id: Date,
+    private name: string,
+    private price: number,
+    private amount
+  ) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.amount = amount;
+  }
+}
